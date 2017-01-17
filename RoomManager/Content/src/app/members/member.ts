6223c695e3613194d48807b37600bd82f0befd64
Ruslan Borovok <borovok.ruslan@outlook.com>
2017-01-17 15:21:48 +0200

    deleted temporary files

---
+++ RoomManager/Content/src/app/members/member.ts
@@ -0,0 +1,4 @@
+export class Member {
+  Id: number;
+  Name: string;
+}
--- RoomManager/Content/src/app/members/members.component.ts
+++ RoomManager/Content/src/app/members/members.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit }      from '@angular/core';
+import { Component, OnInit, Input }      from '@angular/core';
+
+import { Member }       from './member';
+import { Room }         from '../rooms/room';
 
 @Component({
   selector: 'members',
@@ -7,10 +10,13 @@ import { Component, OnInit }      from '@angular/core';
 })
 
 export class MembersComponent implements OnInit{
-
-
+    @Input() selectedRoom: Room;
+    members: Member[];
 
     ngOnInit(): void {
-
+      debugger
+      if(!this.selectedRoom){
+        this.members = 
+      }  
     }
 }
